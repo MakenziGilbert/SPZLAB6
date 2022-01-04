de9a49fce50d49b73b803230c4c3a9575936dafa
Maksym Skorbatiuk <57547446+MakenziGilbert@users.noreply.github.com>
2022-01-04 13:39:26 +0200

    all

---
+++ LabWork6/Factory.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace LabWork6
+{
+    public class Factory : IDisposable
+    {
+        public int Departments { get; }
+        public int Products { get; private set; }
+        public int RawMaterial { get; private set; }
+        public int Money { get; set; }
+        public int FreeStorageSpace { get; private set; } // in percentage 
+
+
+        private FactoryStamp _stamp;
+        private State _state;
+
+        public State CurrentState
+        {
+            get => _state;
+            set
+            {
+                if (value == _state)
+                {
+                    return;
+                }
+
+                var newStamp = new FactoryStamp(this);
+                GenerateReport(_state, _stamp, newStamp);
+                _state = value;
+                _stamp = newStamp;
+            }
+        }
+
+        private static void GenerateReport(State state, FactoryStamp begin, FactoryStamp end)
+        {
+            var format = new XmlSerializer(typeof(FactoryReport));
+            var filename = Directory.GetCurrentDirectory() + "/" + state + "Report";
+            var tmp = filename + ".alxrp";
+            var number = 1;
+            while (File.Exists(tmp))
+            {
+                tmp = filename + number + ".alxrp";
+                number++;
+            }
+
+            filename = tmp;
+            using (var file = new FileStream(filename, FileMode.Create))
+            {
+                format.Serialize(file, new FactoryReport
+                {
+                    StartStamp = begin,
+                    EndStamp = end,
+                    State = state.ToString()
+                });
+            }
+        }
+
+        public bool Runnable { get; set; }
+
+        public event Action PropertiesUpdated;
+
+        private const int MaterialPrice = 5;
+        private const int SailingPrice = 20;
+        private const int CreationPrice = 5;
+        private const int MaterialSpace = 5; // Percentage in storage
+
+        public EventWaitHandle Event { get; }
+        private readonly Thread _worker;
+
+        private const int BuyingTime = 1;
+        private const int CreatingTime = 1;
+        private const int SailingTime = 2;
+
+
+        public Factory()
+        {
+            Runnable = true;
+            CurrentState = State.Buying;
+            Departments = new Random().Next(10) * 5;
+            FreeStorageSpace = 100;
+            Event = new ManualResetEvent(false);
+            _worker = new Thread(Run) {IsBackground = true};
+            _worker.Start();
+        }
+
+        private void Run()
+        {
+            Thread.Sleep(500);
+            _stamp = new FactoryStamp(this);
+            while (Runnable)
+            {
+                Event.WaitOne();
+                switch (CurrentState)
+                {
+                    case State.Buying:
+                    {
+                        BuyMaterial();
+                        Thread.Sleep(BuyingTime * 1000);
+                        break;
+                    }
+                    case State.Creating:
+                    {
+                        CreateProduct();
+                        Thread.Sleep(CreatingTime * 1000);
+                        break;
+                    }
+                    case State.Sailing:
+                    {
+                        SaleProduct();
+                        Thread.Sleep(SailingTime * 1000);
+                        break;
+                    }
+                }
+
+                PropertiesUpdated?.Invoke();
+            }
+        }
+
+
+        private void BuyMaterial()
+        {
+            if (FreeStorageSpace == 0 || Money <= 0)
+            {
+                return;
+            }
+
+            Money -= MaterialPrice;
+            RawMaterial++;
+            FreeStorageSpace -= MaterialSpace;
+        }
+
+        private void CreateProduct()
+        {
+            if (RawMaterial == 0)
+            {
+                return;
+            }
+
+            Money -= CreationPrice;
+            FreeStorageSpace += MaterialSpace;
+            RawMaterial--;
+            Products++;
+        }
+
+        private void SaleProduct()
+        {
+            if (Products == 0)
+            {
+                return;
+            }
+
+            Money += SailingPrice;
+            Products--;
+        }
+
+        public void Dispose() => _worker.Join();
+    }
+
+    public enum State
+    {
+        Buying,
+        Sailing,
+        Creating
+    }
+
+    [Serializable]
+    public struct FactoryStamp
+    {
+        public int Products;
+        public int RawMaterial;
+        public int Money;
+        public int FreeStorageSpace;
+
+        public FactoryStamp(Factory factory)
+        {
+            Products = factory.Products;
+            RawMaterial = factory.RawMaterial;
+            Money = factory.Money;
+            FreeStorageSpace = factory.FreeStorageSpace;
+        }
+    }
+}
---
+++ LabWork6/MainWindow.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LabWork6
+{
+    public partial class MainWindow : Form
+    {
+        private readonly Factory _factory;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenReportDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            OpenReportDialog.Filter = @"alx reports (*.alxrp;*.xml)|*.xml;*.alxrp";
+            _factory = new Factory {Money = 200};
+            DepartmentsLabel.Text = _factory.Departments.ToString();
+            _factory.PropertiesUpdated += UpdateLabels;
+            StateComboBox.SelectedIndex = 0;
+        }
+
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _factory.PropertiesUpdated -= UpdateLabels;
+            _factory.Runnable = false;
+            _factory.Event.Set();
+        }
+
+        private void SuspendBtn_Click(object sender, EventArgs e)
+        {
+            _factory.Event.Reset();
+            ResumeBtn.Enabled = true;
+            SuspendBtn.Enabled = false;
+        }
+
+        private void ResumeBtn_Click(object sender, EventArgs e)
+        {
+            _factory.Event.Set();
+            ResumeBtn.Enabled = false;
+            SuspendBtn.Enabled = true;
+        }
+
+        private void UpdateLabels()
+        {
+            ProductsLabel.Invoke((MethodInvoker) delegate { ProductsLabel.Text = _factory.Products.ToString(); });
+
+            MoneyLabel.Invoke((MethodInvoker) delegate { MoneyLabel.Text = _factory.Money.ToString(); });
+
+            RawMaterialLabel.Invoke((MethodInvoker) delegate
+            {
+                RawMaterialLabel.Text = _factory.RawMaterial.ToString();
+            });
+
+            CurrentStateLabel.Invoke((MethodInvoker) delegate
+            {
+                CurrentStateLabel.Text = _factory.CurrentState.ToString();
+            });
+
+            FreeStorageSpaceLabel.Invoke((MethodInvoker) delegate
+            {
+                FreeStorageSpaceLabel.Text = _factory.FreeStorageSpace.ToString();
+            });
+        }
+
+        private void StateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (StateComboBox.SelectedIndex)
+            {
+                case 0:
+                    _factory.CurrentState = State.Buying;
+                    break;
+                case 1:
+                    _factory.CurrentState = State.Creating;
+                    break;
+                case 2:
+                    _factory.CurrentState = State.Sailing;
+                    break;
+            }
+        }
+
+        private void ReportsBtn_Click(object sender, EventArgs e)
+        {
+            if (OpenReportDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            var fileOpener = new Process();
+            fileOpener.StartInfo.FileName = "explorer";
+            fileOpener.StartInfo.Arguments = "\"" + OpenReportDialog.FileName + "\"";
+            fileOpener.Start();
+        }
+    }
+}
